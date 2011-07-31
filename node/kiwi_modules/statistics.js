a3364605d47fce5a4fe6d7133c776cf04275417c
Jack Allnutt <m2ys4u@Gmail.com>
2011-07-31 10:01:24 +0100

    General tidying up!
    Front.js now (almost) passes jslint, although it still uses eval() and possibly leaks global variables.
    
    Fix for new message sending.

--- node/kiwi_modules/statistics.js
+++ node/kiwi_modules/statistics.js
@@ -5,22 +5,22 @@
 
 var stats = {msgs: 0, topic_changes: 0};
 
-exports.onmsgsend = function(msg, opts){
-	stats.msgs++;
+exports.onmsgsend = function (msg, opts) {
+    stats.msgs++;
 
-	if(msg.msg === '!kiwistats'){
-		msg.msg = 'Messages sent: ' + stats.msgs.toString() + '. ';
-		msg.msg += 'Topics set: ' + stats.topic_changes.toString() + '. ';
+    if (msg.msg === '!kiwistats') {
+        msg.msg = 'Messages sent: ' + stats.msgs.toString() + '. ';
+        msg.msg += 'Topics set: ' + stats.topic_changes.toString() + '. ';
 
-		opts.websocket.sendClientEvent('msg', {nick: msg.target, ident: '', hostname: '', channel: msg.target, msg: msg.msg});
-		return null;
-	}
+        opts.websocket.sendClientEvent('msg', {nick: msg.target, ident: '', hostname: '', channel: msg.target, msg: msg.msg});
+        return null;
+    }
 
-	return msg;
+    return msg;
 }
 
-exports.ontopic = function(topic, opts){
-	stats.topic_changes++;
+exports.ontopic = function (topic, opts) {
+    stats.topic_changes++;
 
-	return topic;
+    return topic;
 }
