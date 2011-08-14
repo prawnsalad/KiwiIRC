d17d2abe6e4a5ac293100a341a997e01585efdbd
Jack Allnutt <m2ys4u@Gmail.com>
2011-08-14 16:48:10 +0100

    fixed small bug relating to buffers

--- node/kiwi.js
+++ node/kiwi.js
@@ -427,7 +427,7 @@ var ircSocketDataHandler = function (data, websocket, ircSocket) {
         ircSocket.holdLast = false;
         ircSocket.held = '';
     }
-    if (data.substr(-1) === '\n') {
+    if (data.substr(-1) !== '\n') {
         ircSocket.holdLast = true;
     }
     data = data.split("\n");         
