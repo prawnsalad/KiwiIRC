16c10630cf432f8e73f90ad5592ab27c51356591
Jack Allnutt <m2ys4u@Gmail.com>
2012-09-21 22:49:34 +0100

    Dramatically simplify http-handler.js. Client directory now configurable in config.json

--- server/http-handler.js
+++ server/http-handler.js
@@ -1,53 +1,28 @@
-var fs          = require('fs'),
-    crypto      = require('crypto');
-    url         = require('url'),
-    _           = require('underscore'),
-    uglify      = require('uglify-js'),
-    jade        = require('jade'),
+var url         = require('url'),
     node_static = require ('node-static');
 
 var HTTPHandler = function (config) {
-    var site = config.site;
-    this.config = config;
-    var files;
-    files = fs.readdirSync('client');
-    if ((typeof site !== 'undefined') && (typeof site === 'string') && (_.include(files, site))) {
-        this.site = site;
-        this.static_file_server = new StaticFileServer(site);
-    }
-    else {
-        this.site = 'default';
-        this.static_file_server = null;
-    }
-    
+    this.static_file_server = new StaticFileServer(config.public_html);
 };
 
 module.exports.HTTPHandler = HTTPHandler;
 
-var default_static_file_server = new node_static.Server('client_backbone/');
-
-var StaticFileServer = function (site) {
-    this.fileServer = new node_static.Server('client_backbone/');
+var StaticFileServer = function (public_html) {
+    public_html = public_html || 'client_backbone/';
+    this.fileServer = new node_static.Server(public_html);
 };
 
 StaticFileServer.prototype.serve = function (request, response) {
     this.fileServer.serve(request, response, function (err) {
         if (err) {
-            default_static_file_server.serve(request, response);
+            response.writeHead(err.status, err.headers);
+            response.end();
         }
     });
 };
 
-var serve_static_file = function (request, response) {
-    if (this.static_file_server !== null) {
-        this.static_file_server.serve(request, response);
-    } else {
-        default_static_file_server.serve(request, response);
-    }
-};
-
 HTTPHandler.prototype.handler = function (request, response) {
-    var file_list, default_file_list, hash, uri, site, subs, self = this;
+    var uri, subs;
     
     uri = url.parse(request.url, true);
     subs = uri.pathname.substr(0, 4);
@@ -55,29 +30,6 @@ HTTPHandler.prototype.handler = function (request, response) {
     if (uri.pathname.substr(0, 10) === '/socket.io') {
         return;
     } else {
-        serve_static_file.call(this, request, response);
+        this.static_file_server.serve(request, response);
     }
-};
-
-var cache = Object.create(null);
-
-var set_cache = function (site, file, data) {
-    if (!cache[site]) {
-        cache[site] = Object.create(null);
-    }
-    var hash = crypto.createHash('md5').update(data).digest('base64');
-    cache[site][file] = {'data': data, 'hash': hash};
-    return hash;
-};
-
-var is_cached = function (site, file) {
-    if ((cache[site]) && (cache[site][file])) {
-        return cache[site][file].hash;
-    } else {
-        return false;
-    }
-};
-
-var get_cache = function (site, file) {
-    return cache[site][file].data;
 };
