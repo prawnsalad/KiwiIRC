e0b01f3c94069daa649f5d2e3eacb29110c2faeb
Darren <darren@darrenwhitlen.com>
2012-11-06 16:24:34 +0000

    Client: Rudimentry objects for third party use

--- client/assets/dev/model_application.js
+++ client/assets/dev/model_application.js
@@ -130,6 +130,13 @@ _kiwi.model.Application = function () {
 
         this.initializeGlobals = function () {
             _kiwi.global.control = this.controlbox;
+            _kiwi.global.gateway = _kiwi.gateway;
+            _kiwi.global.panels = this.panels;
+            
+            _kiwi.global.components = {
+                Applet: _kiwi.model.Applet,
+                Panel: _kiwi.model.Panel
+            };
         };
 
 
