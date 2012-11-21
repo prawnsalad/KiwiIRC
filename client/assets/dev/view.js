43ad0845b7663325807aa8c5935585c1d2f98a69
Darren <darren@darrenwhitlen.com>
2012-11-21 23:12:08 +0000

    Client: Mini theme now in theme selection

--- client/assets/dev/view.js
+++ client/assets/dev/view.js
@@ -1006,7 +1006,7 @@ _kiwi.view.Application = Backbone.View.extend({
 
         // Clear any current theme
         this.$el.removeClass(function (i, css) {
-            return (css.match (/\btheme_\S+/g) || []).join(' ');
+            return (css.match(/\btheme_\S+/g) || []).join(' ');
         });
 
         // Apply the new theme
