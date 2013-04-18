1837e2d6d2f62125d11a679b54dc8815d6074060
Mal Curtis <mal@sitepoint.com>
2013-04-18 12:12:04 +1000

    Live gist, and update template too

--- client/assets/frontpage.js
+++ client/assets/frontpage.js
@@ -1,10 +1,16 @@
 $(function(){
 	$.ajax({
-		url: 'https://api.github.com/gists/5409294',
+		url: 'https://api.github.com/gists/5409396',
 		dataType: 'json', success: function(gist){
 			info = JSON.parse(gist['files']['frontpage.json']['content']);
 			$('#allpages-title').html(info['title']);
 			$('#frontpage-description').html(info['description']);
+			$template = $('#tmpl_server_select');
+			$template.html(
+				$template.
+					html().
+					replace('<!--frontpage-description-->', info['description'])
+			);
 			if(info['hideLogin'] === true){
 				$('.server_details').hide();
 			}
