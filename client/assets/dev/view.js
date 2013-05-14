prawnsalad/KiwiIRC

commit c11bb23d034b86d318d01c84bc109ab7a5890367

ChatBuffer-KeyUp: Suppress browser's default behavior; leave cursor at the end of the line

client/assets/dev/view.js
@@ -1016,7 +1016,8 @@ _kiwi.view.ControlBox = Backbone.View.extend({
                 this.buffer_pos--;
                 inp.val(this.buffer[this.buffer_pos]);
             }
-            break;
+            //suppress browsers default behavior as it would set the coursor at the beginning)
+            return false;
 
         case (ev.keyCode === 40):              // down
             if (this.buffer_pos < this.buffer.length) {

commit 7c77671ce88c8f55490fba3e6e52306d0fdde085

Fixed typo introduced in commit c11bb23

client/assets/dev/view.js
@@ -1016,7 +1016,7 @@ _kiwi.view.ControlBox = Backbone.View.extend({
                 this.buffer_pos--;
                 inp.val(this.buffer[this.buffer_pos]);
             }
-            //suppress browsers default behavior as it would set the coursor at the beginning)
+            //suppress browsers default behavior as it would set the cursor at the beginning
             return false;
 
         case (ev.keyCode === 40):              // down