b11c1422985daf8ef891e224113f0a5b8158da4d
Vlad Chernushevich <mailbox@happytodesign.com>
2013-06-18 01:20:30 +0200

    Convert whitespaces to fix links sticking together

--- client/assets/src/views/controlbox.js
+++ client/assets/src/views/controlbox.js
@@ -226,8 +226,8 @@ _kiwi.view.ControlBox = Backbone.View.extend({
         
         // The default command
         if (command_raw[0] !== '/' || command_raw.substr(0, 2) === '//') {
-            // Remove any slash escaping at the start (ie. //)
-            command_raw = command_raw.replace(/^\/\//, '/');
+            // Remove any slash escaping at the start (ie. //), convert whitespaces to regular
+            command_raw = command_raw.replace(/^\/\//, '/').replace(/\s/, ' ');
 
             // Prepend the default command
             command_raw = '/msg ' + _kiwi.app.panels().active.get('name') + ' ' + command_raw;
