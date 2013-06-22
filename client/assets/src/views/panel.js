a2a409db8a148669ec67d44497b80aa710b24684
Darren <darren@darrenwhitlen.com>
2013-06-22 17:39:43 +0100

    Using CSS to hide repeated nicks

--- client/assets/src/views/panel.js
+++ client/assets/src/views/panel.js
@@ -121,7 +121,7 @@ _kiwi.view.Panel = Backbone.View.extend({
             // Time difference between this message and the last (in minutes)
             time_difference = (template_vars.date.getTime() - prev_msg.date.getTime())/1000/60;
             if (prev_msg.nick === template_vars.nick && time_difference < 1) {
-                template_vars.nick = '';
+                msg_css_classes += ' repeated_nick';
             }
         }
 
