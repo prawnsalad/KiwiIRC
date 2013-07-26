a91621fdb40a7bab92d72d779c4fcf6174e35130
Darren <darren@darrenwhitlen.com>
2013-07-27 00:06:25 +0100

    Debugging console server module

---
+++ server_modules/web_agent_debugger.js
@@ -0,0 +1,22 @@
+/**
+ * Server debugging via node-webkit-agent
+ * https://github.com/c4milo/node-webkit-agent
+ *
+ * Requires npm module: webkit-devtools-agent
+ */
+
+
+var kiwiModules = require('../server/modules'),
+	DevToolsAgent = require('webkit-devtools-agent');
+
+
+var module = new kiwiModules.Module('web_agent_debugger');
+
+var agent = new DevToolsAgent();
+agent.start();
+
+console.log('Debugging can be accessed via http://c4milo.github.io/node-webkit-agent/26.0.1410.65/inspector.html?host=localhost:9999&page=0');
+
+module.on('dispose', function() {
+	agent.stop();
+});
