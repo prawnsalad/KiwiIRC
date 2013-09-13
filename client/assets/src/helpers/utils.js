e62f5be1c9049ec330803bf3ce0b495bbeccef73
Darren <darren@darrenwhitlen.com>
2013-09-14 00:27:16 +0100

    emoticons moved to CSS

--- client/assets/src/helpers/utils.js
+++ client/assets/src/helpers/utils.js
@@ -367,77 +367,81 @@ function emoticonFromText(str) {
     var words_in = str.split(' '),
         words_out = [],
         i,
-        emoticon_location = _kiwi.app.get('base_path') + '/assets/img/emoticons.png',
-        pushEmoticon = function (alt, offset) {
-            words_out.push('<i class="emoticon" style="background-image:url(' + emoticon_location + '); background-position:-' + offset+ 'px 0px">' + alt + '</i>');
+        pushEmoticon = function (alt, emote_name) {
+            words_out.push('<i class="emoticon ' + emote_name + '">' + alt + '</i>');
         };
 
     for (i = 0; i < words_in.length; i++) {
         switch(words_in[i]) {
         case ':)':
-            pushEmoticon(':)', 0);
+            pushEmoticon(':)', 'smile');
             break;
         case ':(':
-            pushEmoticon(':(', 25);
+            pushEmoticon(':(', 'sad');
             break;
         case ':3':
-            pushEmoticon(':3', 50);
+            pushEmoticon(':3', 'lion');
             break;
         case ';3':
-            pushEmoticon(';3', 75);
+            pushEmoticon(';3', 'winky_lion');
             break;
         case ':s':
-            pushEmoticon(':s', 100);
+        case ':S':
+            pushEmoticon(':s', 'confused');
             break;
         case ';(':
-            pushEmoticon(';(', 125);
+        case ';_;':
+            pushEmoticon(';(', 'cry');
             break;
         case ';)':
-            pushEmoticon(';)', 150);
+            pushEmoticon(';)', 'wink');
             break;
         case ';D':
-            pushEmoticon(';D"', 175);
+            pushEmoticon(';D"', 'wink_happy');
             break;
         case ':P':
-            pushEmoticon(':P', 200);
+        case ':p':
+            pushEmoticon(':P', 'tongue');
             break;
         case 'xP':
-            pushEmoticon('xP', 225);
+            pushEmoticon('xP', 'cringe_tongue');
             break;
         case ':o':
         case ':O':
-            pushEmoticon(':o', 250);
+        case ':0':
+            pushEmoticon(':o', 'shocked');
             break;
         case ':D':
-            pushEmoticon(':D', 275);
+            pushEmoticon(':D', 'happy');
             break;
-        case '^^,':
-            pushEmoticon('^^,', 300);
+        case '^^':
+        case '^.^':
+            pushEmoticon('^^,', 'eyebrows');
             break;
         case '&lt;3':
-            pushEmoticon('<3', 325);
-            break;
-        case '&gt;.&lt;':
-            pushEmoticon('>.<', 348);
+            pushEmoticon('<3', 'heart');
             break;
         case '&gt;_&lt;':
-            pushEmoticon('>_<', 373);
+        case '&gt;.&lt;':
+            pushEmoticon('>_<', 'doh');
             break;
         case 'XD':
         case 'xD':
-            pushEmoticon('xD', 398);
+            pushEmoticon('xD', 'big_grin');
             break;
         case 'o.0':
-            pushEmoticon('o.0', 423);
+        case 'o.O':
+            pushEmoticon('o.0', 'wide_eye_right');
             break;
         case '0.o':
-            pushEmoticon('0.o', 448);
+        case 'O.o':
+            pushEmoticon('0.o', 'wide_eye_left');
             break;
         case ':\\':
         case '=\\':
         case ':/':
         case '=/':
-            pushEmoticon(':\\', 473);
+            pushEmoticon(':\\', 'unsure');
             break;
         default:
             words_out.push(words_in[i]);
