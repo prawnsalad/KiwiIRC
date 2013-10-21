b0d5c67c7e62bdb983497e7cc5c594b974a2589a
Darren <darren@darrenwhitlen.com>
2013-10-21 22:15:29 +0100

    Persistence storage now using callbacks instead of returns

--- server_modules/persistence.js
+++ server_modules/persistence.js
@@ -6,33 +6,34 @@ var kiwiModules = require('../server/modules'),
     express = require('express'),
     express_app = express();
 
-
+// TODO: Add password hashing (username:password:salt)
 var StorageMemory = function() {
     this.user_states = {};
 };
-StorageMemory.prototype.userExists = function(username) {
-    return !!(this.user_states[username]);
+StorageMemory.prototype.userExists = function(username, callback) {
+    callback(!!(this.user_states[username]));
 };
-StorageMemory.prototype.getUserState = function(username, password) {
+StorageMemory.prototype.getUserState = function(username, password, callback) {
     var user, state;
 
-    if (!this.userExists(username))
-        return false;
+    // Check if this user exists
+    if (!this.user_states[username])
+        return callback(false);
 
     user = this.user_states[username];
 
     if (user.password !== password)
-        return false;
+        return callback(false);
 
     state = global.states.getState(user.state_id);
     if (!state)
-        return false;
+        return callback(false);
 
     user.last_used = new Date();
 
-    return state;
+    return callback(state);
 };
-StorageMemory.prototype.setUserState = function(username, password, state) {
+StorageMemory.prototype.setUserState = function(username, password, state, callback) {
     this.user_states[username] = this.user_states[username] || {};
     var user = this.user_states[username];
 
@@ -40,6 +41,8 @@ StorageMemory.prototype.setUserState = function(username, password, state) {
     user.password = password;
     user.last_used = new Date();
     user.state_id = state.hash;
+
+    callback();
 };
 
 
@@ -58,79 +61,85 @@ module.on('client created', function (event, event_data) {
 
 
 module.on('client command kiwi', function(event, event_data) {
-    var state, username, password;
+    var username, password;
 
     switch(event_data.command.command){
         case 'session_save':
+            event.preventDefault();
+
             username = event_data.command.username;
             password = event_data.command.password;
 
             // If this user already has a state, kill and replace it
-            state = storage.getUserState(username, password);
-            if (state) {
-                state.save_state = false;
-                state.dispose();
-            }
+            storage.getUserState(username, password, function(state) {
+                if (state) {
+                    state.save_state = false;
+                    state.dispose();
+                }
 
-            storage.setUserState(username, password, event_data.client.state);
-            event_data.client.state.save_state = true;
+                storage.setUserState(username, password, event_data.client.state, function() {
+                    event_data.client.state.save_state = true;
 
-            event_data.callback(null, 'Sate will now persist');
+                    event_data.callback(null, 'Sate will now persist');
+                });
+            });
 
-            event.preventDefault();
             break;
 
         case 'session_resume':
             var connections = [];
 
+            event.preventDefault();
+
             username = event_data.command.username;
             password = event_data.command.password;
-            state = storage.getUserState(username, password);
 
-            if (!state) {
-                event_data.callback('does_not_exist');
-                return;
-            }
-
-            if (event_data.client.state)
-                event_data.client.state.dispose();
-
-            state.addClient(event_data.client);
-            event_data.client.state = state;
-
-            // First loop - compile a list of connection information
-            _.each(state.irc_connections, function(irc_connection) {
-                if (!irc_connection)
+            storage.getUserState(username, password, function(state) {
+                if (!state) {
+                    event_data.callback('does_not_exist');
                     return;
+                }
 
-                connections.push({
-                    connection_id: irc_connection.con_num,
-                    options: irc_connection.server.cache.options || {},
-                    nick: irc_connection.nick,
-                    address: irc_connection.irc_host.hostname,
-                    port: irc_connection.irc_host.port,
-                    ssl: irc_connection.ssl
+                if (event_data.client.state)
+                    event_data.client.state.dispose();
+
+                state.addClient(event_data.client);
+                event_data.client.state = state;
+
+                // First loop - compile a list of connection information
+                _.each(state.irc_connections, function(irc_connection) {
+                    if (!irc_connection)
+                        return;
+
+                    connections.push({
+                        connection_id: irc_connection.con_num,
+                        options: irc_connection.server.cache.options || {},
+                        nick: irc_connection.nick,
+                        address: irc_connection.irc_host.hostname,
+                        port: irc_connection.irc_host.port,
+                        ssl: irc_connection.ssl
+                    });
+                });
+
+                // Send the connection information to the client
+                event_data.client.sendKiwiCommand('connection_sync', connections);
+
+                // Second loop - Now the client has the info, sync some data
+                _.each(state.irc_connections, function(irc_connection) {
+                    if (!irc_connection)
+                        return;
+
+                    // Get a fresh MOTD sent to the client
+                    irc_connection.write('MOTD');
+
+                    _.each(irc_connection.irc_channels, function(chan, chan_name) {
+                        chan.refreshNickList();
+                        chan.refreshTopic();
+                    });
                 });
             });
 
-            // Send the connection information to the client
-            event_data.client.sendKiwiCommand('connection_sync', connections);
 
-            // Second loop - Now the client has the info, sync some data
-            _.each(state.irc_connections, function(irc_connection) {
-                if (!irc_connection)
-                    return;
-
-                // Get a fresh MOTD sent to the client
-                irc_connection.write('MOTD');
-
-                _.each(irc_connection.irc_channels, function(chan, chan_name) {
-                    chan.refreshNickList();
-                    chan.refreshTopic();
-                });
-            });
-
-            event.preventDefault();
             break;
     }
 });
