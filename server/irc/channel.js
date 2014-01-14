6c7b20067cfe035817d6eec068acfb6a331d2691
Darren <darren@darrenwhitlen.com>
2014-01-14 22:44:05 +0000

    Server module hooks for IRC events

--- server/irc/channel.js
+++ server/irc/channel.js
@@ -43,73 +43,125 @@ IrcChannel.prototype.dispose = function (){
 
 
 function onJoin(event) {
-    this.irc_connection.clientEvent('join', {
-        channel: this.name,
-        nick: event.nick,
-        ident: event.ident,
-        hostname: event.hostname,
-        time: event.time
+    var that = this;
+
+    global.modules.emit('irc channel join', {
+        channel: this,
+        connection: this.irc_connection,
+        irc_event: event
+    })
+    .done(function() {
+        that.irc_connection.clientEvent('join', {
+            channel: that.name,
+            nick: event.nick,
+            ident: event.ident,
+            hostname: event.hostname,
+            time: event.time
+        });
     });
 }
 
 
 function onPart(event) {
-    this.irc_connection.clientEvent('part', {
-        nick: event.nick,
-        ident: event.ident,
-        hostname: event.hostname,
-        channel: this.name,
-        message: event.message,
-        time: event.time
+    var that = this;
+
+    global.modules.emit('irc channel part', {
+        channel: this,
+        connection: this.irc_connection,
+        irc_event: event
+    })
+    .done(function() {
+        that.irc_connection.clientEvent('part', {
+            nick: event.nick,
+            ident: event.ident,
+            hostname: event.hostname,
+            channel: that.name,
+            message: event.message,
+            time: event.time
+        });
     });
 }
 
 
 function onKick(event) {
-    this.irc_connection.clientEvent('kick', {
-        kicked: event.kicked,  // Nick of the kicked
-        nick: event.nick, // Nick of the kicker
-        ident: event.ident,
-        hostname: event.hostname,
-        channel: this.name,
-        message: event.message,
-        time: event.time
+    global.modules.emit('irc channel kick', {
+        channel: this,
+        connection: this.irc_connection,
+        irc_event: event
+    })
+    .done(function() {
+        this.irc_connection.clientEvent('kick', {
+            kicked: event.kicked,  // Nick of the kicked
+            nick: event.nick, // Nick of the kicker
+            ident: event.ident,
+            hostname: event.hostname,
+            channel: this.name,
+            message: event.message,
+            time: event.time
+        });
     });
 }
 
 
 function onQuit(event) {
-    this.irc_connection.clientEvent('quit', {
-        nick: event.nick,
-        ident: event.ident,
-        hostname: event.hostname,
-        message: event.message,
-        time: event.time
+    var that = this;
+
+    global.modules.emit('irc channel quit', {
+        channel: this,
+        connection: this.irc_connection,
+        irc_event: event
+    })
+    .done(function() {
+        that.irc_connection.clientEvent('quit', {
+            nick: event.nick,
+            ident: event.ident,
+            hostname: event.hostname,
+            message: event.message,
+            time: event.time
+        });
     });
 }
 
 
 function onMsg(event) {
-    this.irc_connection.clientEvent('msg', {
-        nick: event.nick,
-        ident: event.ident,
-        hostname: event.hostname,
-        channel: this.name,
-        msg: event.msg,
-        time: event.time
+    var that = this;
+
+    global.modules.emit('irc message', {
+        channel: this,
+        connection: this.irc_connection,
+        irc_event: event
+    })
+    .done(function() {
+        that.irc_connection.clientEvent('msg', {
+            nick: event.nick,
+            ident: event.ident,
+            hostname: event.hostname,
+            channel: that.name,
+            msg: event.msg,
+            time: event.time
+        });
     });
 }
 
 
 function onNotice(event) {
-    this.irc_connection.clientEvent('notice', {
-        from_server: event.from_server,
-        nick: event.nick,
-        ident: event.ident,
-        hostname: event.hostname,
-        target: event.target,
-        msg: event.msg,
-        time: event.time
+    var that = this;
+
+    global.modules.emit('irc channel notice', {
+        channel: this,
+        connection: this.irc_connection,
+        irc_event: event
+    })
+    .done(function() {
+        that.irc_connection.clientEvent('notice', {
+            from_server: event.from_server,
+            nick: event.nick,
+            ident: event.ident,
+            hostname: event.hostname,
+            target: event.target,
+            msg: event.msg,
+            time: event.time
+        });
     });
 }
 
@@ -173,11 +225,20 @@ function updateUsersList(users) {
 
 
 function onTopic(event) {
-    this.irc_connection.clientEvent('topic', {
-        nick: event.nick,
-        channel: this.name,
-        topic: event.topic,
-        time: event.time
+    var that = this;
+
+    global.modules.emit('irc channel topic', {
+        channel: this,
+        connection: this.irc_connection,
+        irc_event: event
+    })
+    .done(function() {
+        that.irc_connection.clientEvent('topic', {
+            nick: event.nick,
+            channel: that.name,
+            topic: event.topic,
+            time: event.time
+        });
     });
 }
 
@@ -203,13 +264,6 @@ function onBanListEnd(event) {
     this.ban_list_buffer = [];
 }
 
-function onTopic(event) {
-    this.irc_connection.clientEvent('topic', {
-        channel: event.channel,
-        topic: event.topic
-    });
-}
-
 function onTopicSetBy(event) {
     this.irc_connection.clientEvent('topicsetby', {
         nick: event.nick,
@@ -219,10 +273,19 @@ function onTopicSetBy(event) {
 }
 
 function onMode(event) {
-    this.irc_connection.clientEvent('mode', {
-        target: event.target,
-        nick: event.nick,
-        modes: event.modes,
-        time: event.time
+    var that = this;
+
+    global.modules.emit('irc channel mode', {
+        channel: this,
+        connection: this.irc_connection,
+        irc_event: event
+    })
+    .done(function() {
+        that.irc_connection.clientEvent('mode', {
+            target: event.target,
+            nick: event.nick,
+            modes: event.modes,
+            time: event.time
+        });
     });
 }
