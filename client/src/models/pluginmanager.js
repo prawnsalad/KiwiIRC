6b8fbed0c4a08bf883e95cc777977c7acf16e6e6
Darren <darren@darrenwhitlen.com>
2014-03-31 21:34:31 +0100

    Show the app after plugins loaded; Allow custom startup applets;

--- client/src/models/pluginmanager.js
+++ client/src/models/pluginmanager.js
@@ -2,18 +2,24 @@ _kiwi.model.PluginManager = Backbone.Model.extend({
     initialize: function () {
         this.$plugin_holder = $('<div id="kiwi_plugins" style="display:none;"></div>')
             .appendTo(_kiwi.app.view.$el);
+
+        this.loading_plugins = 0;
         this.loaded_plugins = {};
     },
 
     // Load an applet within this panel
     load: function (url) {
+        var that = this;
+
         if (this.loaded_plugins[url]) {
             this.unload(url);
         }
 
+        this.loading_plugins++;
+
         this.loaded_plugins[url] = $('<div></div>');
         this.loaded_plugins[url].appendTo(this.$plugin_holder)
-            .load(url);
+            .load(url, _.bind(that.pluginLoaded, that));
     },
 
 
@@ -24,5 +30,15 @@ _kiwi.model.PluginManager = Backbone.Model.extend({
 
         this.loaded_plugins[url].remove();
         delete this.loaded_plugins[url];
-    }
+    },
+
+
+    // Called after each plugin is loaded
+    pluginLoaded: function() {
+        this.loading_plugins--;
+
+        if (this.loading_plugins === 0) {
+            this.trigger('loaded');
+        }
+    },
 });
