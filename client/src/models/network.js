8c90d1dcb563d02399fcea33fff5378883d7cebf
Darren <darren@darrenwhitlen.com>
2014-06-01 12:30:16 +0100

    Typo

--- client/src/models/network.js
+++ client/src/models/network.js
@@ -128,7 +128,7 @@
             this.gateway.on('channel:join', onJoin, this);
             this.gateway.on('channel:part', onPart, this);
             this.gateway.on('channel:quit', onQuit, this);
-            this.gateway.on('channe:kick', onKick, this);
+            this.gateway.on('channel:kick', onKick, this);
             this.gateway.on('message', onMessage, this);
             this.gateway.on('nick', onNick, this);
             this.gateway.on('ctcp_request', onCtcpRequest, this);
