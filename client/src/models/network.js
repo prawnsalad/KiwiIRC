7a6ef4aa3d3589c86dec6404aed97c6bb8d202e2
Jack Allnutt <jack@allnutt.eu>
2014-06-04 22:37:11 +0100

    Fixed additional host -> hostname

--- client/src/models/network.js
+++ client/src/models/network.js
@@ -734,7 +734,7 @@
             return;
 
         panel = _kiwi.app.panels().active;
-        if (event.host) {
+        if (event.hostname) {
             panel.addMsg(event.nick, styleText('who', {nick: event.nick, ident: event.ident, host: event.hostname, realname: event.real_name, text: event.msg}), 'whois');
         } else {
             panel.addMsg(event.nick, styleText('whois_notfound', {nick: event.nick, text: translateText('client_models_network_nickname_notfound', [])}), 'whois');
