7f272ac9f56471ac4fc7030cc767cc2a1d335bfd
Jack Allnutt <jack@allnutt.eu>
2014-06-08 09:28:16 +0100

    Add message param to irc.part RPC command (as per #509)

--- client/src/models/gateway.js
+++ client/src/models/gateway.js
@@ -348,11 +348,20 @@ _kiwi.model.Gateway = function () {
     /**
     *   Leaves a channel
     *   @param  {String}    channel     The channel to part
+    *   @param  {String}    message     Optional part message
     *   @param  {Function}  callback    A callback function
     */
-    this.part = function (connection_id, channel, callback) {
+    this.part = function (connection_id, channel, message, callback) {
+        "use strict";
+
+        // The message param is optional, so juggle args if it is missing
+        if (typeof arguments[2] === 'function') {
+            callback = arguments[2];
+            message = undefined;
+        }
         var args = {
-            channel: channel
+            channel: channel,
+            message: message
         };
 
         this.rpcCall('irc.part', connection_id, args, callback);
