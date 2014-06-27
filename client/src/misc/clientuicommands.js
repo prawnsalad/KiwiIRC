04c7ce91f1d90ebdeb763164652de46ce620e45f
Darren <darren@darrenwhitlen.com>
2014-06-27 23:47:14 +0100

    /cycle command #444

--- client/src/misc/clientuicommands.js
+++ client/src/misc/clientuicommands.js
@@ -65,6 +65,7 @@
         'command:action':      actionCommand,
         'command:join':        joinCommand,
         'command:part':        partCommand,
+        'command:cycle':        cycleCommand,
         'command:nick':        nickCommand,
         'command:query':       queryCommand,
         'command:invite':      inviteCommand,
@@ -298,6 +299,27 @@
                 that.connections.active_connection.gateway.part(channel, msg);
             });
         }
+    }
+
+
+    function cycleCommand (ev) {
+        var that = this,
+            chan_name;
+
+        if (ev.params.length === 0) {
+            chan_name = this.app.panels().active.get('name');
+        } else {
+            chan_name = ev.params[0];
+        }
+
+        this.app.connections.active_connection.gateway.part(chan_name);
+
+        // Wait for a second to give the network time to register the part command
+        setTimeout(function() {
+            // Use createAndJoinChannels() here as it auto-creates panels instead of waiting for the network
+            that.app.connections.active_connection.createAndJoinChannels(chan_name);
+            that.app.connections.active_connection.panels.getByName(chan_name).show();
+        }, 1000);
     }
 
 
