5ee8c6b9a2c5a12b3c2a2c0002891d10722a8e0c
Darren <darren@darrenwhitlen.com>
2014-08-16 00:30:58 +0100

    Syncing individual channels

--- client/src/models/session.js
+++ client/src/models/session.js
@@ -4,6 +4,7 @@ _kiwi.model.Session = Backbone.Model.extend({
     },
 
 	initialize: function() {
+        this.available_connections = new Backbone.Collection();
 	},
 
 
@@ -40,11 +41,37 @@ _kiwi.model.Session = Backbone.Model.extend({
 
 
 	_resumeCallback: function(err, data) {
+        var that = this;
+
 		if (err) {
 			this.trigger('sync_error', err);
 			return;
 		}
 
+        _.each(data, function(connection) {
+            var con = new Backbone.Model({
+                connection_id: connection.connection_id,
+                nick: connection.nick,
+                address: connection.host,
+                port: connection.port,
+                ssl: connection.ssl,
+                options: connection.options
+            });
+
+            var channels = new Backbone.Collection();
+            _.each(connection.channels, function(channel_info, idx) {
+                channels.add(new Backbone.Model({name: channel_info.name}));
+            });
+
+            con.set('channels', channels);
+
+            that.available_connections.add(con);
+        });
+
+        this.set('synced', true);
+        this.trigger('synced', data);
+
+/*
         // For each connection, create the network object and channels
         _.each(data, function(connection) {
             var new_connection,
@@ -79,21 +106,65 @@ _kiwi.model.Session = Backbone.Model.extend({
                 nick: connection.nick
             });
         });
-
-        this.set('synced', true);
-		this.trigger('synced', data);
+*/
 	},
 
 
     syncEvents: function(network_id, target, callback) {
-        if (target && !callback) {
+        if (typeof target === 'function' && !callback) {
             callback = target;
             target = undefined;
         }
+
+        var connection = this.available_connections.findWhere({connection_id: network_id});
+        if (!connection) {
+            return false;
+        }
+
+        if (!connection.synced) {
+            this._syncConnection(connection);
+        }
+        connection.synced = true;
 
         _kiwi.gateway.rpc.call('kiwi.session_events', {
             connection_id: network_id,
             target: target,
         }, callback);
     },
+
+
+    _syncConnection: function(connection) {
+        var new_connection, options;
+
+        new_connection = new _kiwi.model.Network({
+            connection_id: connection.get('connection_id'),
+            nick: connection.get('nick'),
+            address: connection.get('host'),
+            port: connection.get('port'),
+            ssl: connection.get('.ssl')
+        });
+
+        options = connection.get('options');
+        _kiwi.gateway.trigger('connection:' + connection.get('connection_id').toString(), {
+            event_name: 'options',
+            event_data: {options: options, cap: options.cap}
+        });
+
+        _kiwi.app.connections.add(new_connection);
+/*
+        _.each(connection.channels, function(channel_info, idx) {
+            var channel = new_connection.panels.getByName(channel_info.name);
+
+            if (!channel) {
+                channel = new _kiwi.model.Channel({name: channel_info.name, network: new_connection});
+                new_connection.panels.add(channel);
+            }
+        });
+*/
+        // Let the application know we have connected to an IRCd
+        _kiwi.gateway.trigger('connection:connect', {
+            server: connection.get('connection_id'),
+            nick: connection.get('nick')
+        });
+    }
 });
