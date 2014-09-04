a9daf332bd6f4bd7df3d28014e72642582959445
Darren <darren@darrenwhitlen.com>
2014-09-04 21:13:04 +0100

    web_agent_debugger_update

--- server_modules/web_agent_debugger.js
+++ server_modules/web_agent_debugger.js
@@ -7,12 +7,11 @@
 
 
 var kiwiModules = require('../server/modules'),
-	DevToolsAgent = require('webkit-devtools-agent');
+	agent = require('webkit-devtools-agent');
 
 
 var module = new kiwiModules.Module('web_agent_debugger');
 
-var agent = new DevToolsAgent();
 agent.start();
 
 console.log('Debugging can be accessed via http://c4milo.github.io/node-webkit-agent/26.0.1410.65/inspector.html?host=localhost:9999&page=0');
