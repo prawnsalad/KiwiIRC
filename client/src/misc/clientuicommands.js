78f6dfd5ff9e0422c73ad86961e29ceea43ca4f4
Darren <darren@darrenwhitlen.com>
2014-10-23 15:01:48 +0100

    Remove % formatting in sent messages. Leave that to plugins.

--- client/src/misc/clientuicommands.js
+++ client/src/misc/clientuicommands.js
@@ -272,7 +272,7 @@
             panel = this.app.connections.active_connection.panels.getByName(destination) || this.app.panels().server;
 
         ev.params.shift();
-        message = formatToIrcMsg(ev.params.join(' '));
+        message = ev.params.join(' ');
 
         panel.addMsg(this.app.connections.active_connection.get('nick'), styleText('privmsg', {text: message}), 'privmsg');
         this.app.connections.active_connection.gateway.msg(destination, message);
