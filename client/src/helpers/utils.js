2b07f86a885eaa317f24282f780e259cdac54c45
jasonb <contact@jasonb.io>
2014-11-22 04:39:20 -0700

    alternate emoticon function
    Different take on the emoticonFromText function. Would be useful for
    those with a ton of emotes because they wouldn't have to manually enter
    every one. Also good for updating emoticons list without needing the
    user's cache to clear first to grab new code

--- client/src/helpers/utils.js
+++ client/src/helpers/utils.js
@@ -412,88 +412,33 @@ function formatCensorMsg(str) {
     return words_out.join(' ');
 }
 
+// alternate aproach to emotes. takes user specified input and compares against all images found in a directory
+// if matching image found, it shows, if not, nothing happens. assumes :code: is used to denote emotes - modify to whatever suits you
 function emoticonFromText(str) {
     var words_in = str.split(' '),
         words_out = [],
         i,
-        pushEmoticon = function (alt, emote_name) {
-            words_out.push('<i class="emoticon ' + emote_name + '">' + alt + '</i>');
+        pattern = /([\s]?([:]+[A-Za-z0-9]+[:]+)[\s]?)/ig,
+        pushEmoticon = function (code) {
+            code2 = code.replace(/:/g, '');
+            code2 = code2.toLowerCase();
+            words_out.push(' <img class="emote" src="images/emotes/'+code2+'.png" alt=":'+code2+':" title=":'+code2+':" onerror="this.style.visibility=\'hidden\';" /> ');
         };
 
     for (i = 0; i < words_in.length; i++) {
         switch(words_in[i]) {
-        case ':)':
-            pushEmoticon(':)', 'smile');
-            break;
-        case ':(':
-            pushEmoticon(':(', 'sad');
-            break;
-        case ':3':
-            pushEmoticon(':3', 'lion');
-            break;
-        case ';3':
-            pushEmoticon(';3', 'winky_lion');
-            break;
-        case ':s':
-        case ':S':
-            pushEmoticon(':s', 'confused');
-            break;
-        case ';(':
-        case ';_;':
-            pushEmoticon(';(', 'cry');
-            break;
-        case ';)':
-            pushEmoticon(';)', 'wink');
-            break;
-        case ';D':
-            pushEmoticon(';D', 'wink_happy');
-            break;
-        case ':P':
-        case ':p':
-            pushEmoticon(':P', 'tongue');
-            break;
-        case 'xP':
-            pushEmoticon('xP', 'cringe_tongue');
-            break;
-        case ':o':
-        case ':O':
-        case ':0':
-            pushEmoticon(':o', 'shocked');
-            break;
-        case ':D':
-            pushEmoticon(':D', 'happy');
-            break;
-        case '^^':
-        case '^.^':
-            pushEmoticon('^^,', 'eyebrows');
-            break;
-        case '&lt;3':
-            pushEmoticon('<3', 'heart');
-            break;
-        case '&gt;_&lt;':
-        case '&gt;.&lt;':
-            pushEmoticon('>_<', 'doh');
-            break;
-        case 'XD':
-        case 'xD':
-            pushEmoticon('xD', 'big_grin');
-            break;
-        case 'o.0':
-        case 'o.O':
-            pushEmoticon('o.0', 'wide_eye_right');
-            break;
-        case '0.o':
-        case 'O.o':
-            pushEmoticon('0.o', 'wide_eye_left');
-            break;
-        case ':\\':
-        case '=\\':
-        case ':/':
-        case '=/':
-            pushEmoticon(':\\', 'unsure');
-            break;
-        default:
-            words_out.push(words_in[i]);
+            case ':placeholder:':
+                code = words_in[i].replace(/:/g, '');
+                code = code.toLowerCase();
+                pushEmoticon(code);
+                break;
+            default:
+                if (pattern.test(words_in[i])) {
+                    code = words_in[i];
+                    pushEmoticon(code);
+                } else {
+                    words_out.push(words_in[i]);
+                }
         }
     }
 
