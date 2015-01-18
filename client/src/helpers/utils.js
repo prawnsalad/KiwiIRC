a2127d40ee3fc4d86d3e404cbf276aa5234d6625
Cory Chaplin <cory.chaplin@laposte.net>
2015-01-18 23:46:54 +0100

    Fix ident bug in text themes

--- client/src/helpers/utils.js
+++ client/src/helpers/utils.js
@@ -219,7 +219,7 @@ function hsl2rgb(h, s, l) {
  */
 function formatToIrcMsg(message) {
     // Format any colour codes (eg. $c4)
-    message = message.replace(/%C(\d)/ig, function(match, colour_number) {
+    message = message.replace(/%C(\d)/g, function(match, colour_number) {
         return String.fromCharCode(3) + colour_number.toString();
     });
 
@@ -229,7 +229,7 @@ function formatToIrcMsg(message) {
         U: '\x1F',    // Underline
         O: '\x0F'     // Out / Clear formatting
     };
-    message = message.replace(/%([BIUO])/ig, function(match, format_code) {
+    message = message.replace(/%([BIUO])/g, function(match, format_code) {
         if (typeof formatters[format_code.toUpperCase()] !== 'undefined')
             return formatters[format_code.toUpperCase()];
     });
