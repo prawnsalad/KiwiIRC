226cb3b5868a84d27056df0f30bc14a5fcf111b3
Darren <darren@darrenwhitlen.com>
2015-07-22 22:55:27 +0100

    Autocomplete command descriptions now in translations

--- client/src/misc/clientuicommands.js
+++ client/src/misc/clientuicommands.js
@@ -5,7 +5,7 @@
         this.controlbox = controlbox;
 
         this.addDefaultAliases();
-        this.bindCommand(fn_to_bind);
+        this.bindCommand(buildCommandFunctions());
     }
 
     _kiwi.misc.ClientUiCommands = ClientUiCommands;
@@ -71,185 +71,190 @@
      * Default functions to bind to controlbox events
      **/
 
-    var fn_to_bind = {
-        'unknown_command':     unknownCommand,
-        'command':             allCommands,
-        'command:msg':         {fn: msgCommand, description: 'Send a message'},
-        'command:action':      {fn: actionCommand, description: 'Do something physical'},
-        'command:join':        {fn: joinCommand, description: 'Join a channel'},
-        'command:part':        {fn: partCommand, description: 'Leave a channel'},
-        'command:cycle':       {fn: cycleCommand, description: 'Leave, then re-join a channel'},
-        'command:nick':        {fn: nickCommand, description: 'Change your nickname'},
-        'command:query':       {fn: queryCommand, description: 'Start a private message with someone'},
-        'command:invite':      {fn: inviteCommand, description: 'Invite somebody into the channel'},
-        'command:topic':       {fn: topicCommand, description: 'Set the topic for this channel'},
-        'command:notice':      {fn: noticeCommand, description: 'Send a notice'},
-        'command:quote':       {fn: quoteCommand, description: 'Send a raw command to the IRC server'},
-        'command:kick':        {fn: kickCommand, description: 'Kick sombody from the channel'},
-        'command:clear':       {fn: clearCommand, description: 'Clear all messages from this window'},
-        'command:ctcp':        {fn: ctcpCommand, description: 'Send a CTCP command to somebody'},
-        'command:quit':        {fn: quitCommand, description: 'Disconnect from the IRC server'},
-        'command:server':      {fn: serverCommand, description: 'Conenct to a new IRC network'},
-        'command:whois':       {fn: whoisCommand, description: 'Request information on somebody'},
-        'command:whowas':      {fn: whowasCommand, description: 'Request information on somebody that disconnected recently'},
-        'command:away':        {fn: awayCommand, description: 'Mark yourself as away'},
-        'command:encoding':    {fn: encodingCommand, description: 'Change your connection encoding'},
-        'command:channel':     channelCommand,
-        'command:applet':      appletCommand,
-        'command:settings':    {fn: settingsCommand, description: 'Show the settings window'},
-        'command:script':      {fn: scriptCommand, description: 'Modify kiwi user scripts'}
-    };
+    function buildCommandFunctions() {
+        var fn_to_bind = {
+            'unknown_command':     unknownCommand,
+            'command':             allCommands,
+            'command:msg':         {fn: msgCommand, description: translateText('command_description_msg')},
+            'command:action':      {fn: actionCommand, description: translateText('command_description_action')},
+            'command:join':        {fn: joinCommand, description: translateText('command_description_join')},
+            'command:part':        {fn: partCommand, description: translateText('command_description_part')},
+            'command:cycle':       {fn: cycleCommand, description: translateText('command_description_cycle')},
+            'command:nick':        {fn: nickCommand, description: translateText('command_description_nick')},
+            'command:query':       {fn: queryCommand, description: translateText('command_description_query')},
+            'command:invite':      {fn: inviteCommand, description: translateText('command_description_invite')},
+            'command:topic':       {fn: topicCommand, description: translateText('command_description_topic')},
+            'command:notice':      {fn: noticeCommand, description: translateText('command_description_notice')},
+            'command:quote':       {fn: quoteCommand, description: translateText('command_description_quote')},
+            'command:kick':        {fn: kickCommand, description: translateText('command_description_kick')},
+            'command:clear':       {fn: clearCommand, description: translateText('command_description_clear')},
+            'command:ctcp':        {fn: ctcpCommand, description: translateText('command_description_ctcp')},
+            'command:quit':        {fn: quitCommand, description: translateText('command_description_quit')},
+            'command:server':      {fn: serverCommand, description: translateText('command_description_server')},
+            'command:whois':       {fn: whoisCommand, description: translateText('command_description_whois')},
+            'command:whowas':      {fn: whowasCommand, description: translateText('command_description_whowas')},
+            'command:away':        {fn: awayCommand, description: translateText('command_description_away')},
+            'command:encoding':    {fn: encodingCommand, description: translateText('command_description_encoding')},
+            'command:channel':     channelCommand,
+            'command:applet':      appletCommand,
+            'command:settings':    {fn: settingsCommand, description: translateText('command_description_settings')},
+            'command:script':      {fn: scriptCommand, description: translateText('command_description_script')}
+        };
 
 
-    fn_to_bind['command:css'] = {
-        description: 'Reload the pages stylesheets',
-        fn: function(ev) {
-            var queryString = '?reload=' + new Date().getTime();
-            $('link[rel="stylesheet"]').each(function () {
-                this.href = this.href.replace(/\?.*|$/, queryString);
-            });
-        }
-    };
-
-
-    fn_to_bind['command:js'] = {
-        description: 'Load a javascript file from a URL',
-        fn: function(ev) {
-            if (!ev.params[0]) return;
-            $script(ev.params[0] + '?' + (new Date().getTime()));
-        }
-    };
-
-
-    fn_to_bind['command:set'] = {
-        description: 'Set a kiwi config setting',
-        fn: function(ev) {
-            if (!ev.params[0]) return;
-
-            var setting = ev.params[0],
-                value;
-
-            // Do we have a second param to set a value?
-            if (ev.params[1]) {
-                ev.params.shift();
-
-                value = ev.params.join(' ');
-
-                // If we're setting a true boolean value..
-                if (value === 'true')
-                    value = true;
-
-                // If we're setting a false boolean value..
-                if (value === 'false')
-                    value = false;
-
-                // If we're setting a number..
-                if (parseInt(value, 10).toString() === value)
-                    value = parseInt(value, 10);
-
-                _kiwi.global.settings.set(setting, value);
-            }
-
-            // Read the value to the user
-            this.app.panels().active.addMsg('', styleText('set_setting', {text: setting + ' = ' + _kiwi.global.settings.get(setting).toString()}));
-        }
-    };
-
-
-    fn_to_bind['command:save'] = {
-        description: 'Save the current kiwi settings',
-        fn: function(ev) {
-            _kiwi.global.settings.save();
-            this.app.panels().active.addMsg('', styleText('settings_saved', {text: translateText('client_models_application_settings_saved')}));
-        }
-    };
-
-
-    fn_to_bind['command:alias'] = {
-        description: 'Create an alias to an existing /command',
-        fn: function(ev) {
-            var that = this,
-                name, rule;
-
-            // No parameters passed so list them
-            if (!ev.params[1]) {
-                $.each(this.controlbox.preprocessor.aliases, function (name, rule) {
-                    that.app.panels().server.addMsg(' ', styleText('list_aliases', {text: name + '   =>   ' + rule}));
+        fn_to_bind['command:css'] = {
+            description: translateText('command_description_css'),
+            fn: function(ev) {
+                var queryString = '?reload=' + new Date().getTime();
+                $('link[rel="stylesheet"]').each(function () {
+                    this.href = this.href.replace(/\?.*|$/, queryString);
                 });
-                return;
             }
+        };
 
-            // Deleting an alias?
-            if (ev.params[0] === 'del' || ev.params[0] === 'delete') {
-                name = ev.params[1];
-                if (name[0] !== '/') name = '/' + name;
-                delete this.controlbox.preprocessor.aliases[name];
-                return;
+
+        fn_to_bind['command:js'] = {
+            description: translateText('command_description_js'),
+            fn: function(ev) {
+                if (!ev.params[0]) return;
+                $script(ev.params[0] + '?' + (new Date().getTime()));
             }
-
-            // Add the alias
-            name = ev.params[0];
-            ev.params.shift();
-            rule = ev.params.join(' ');
-
-            // Make sure the name starts with a slash
-            if (name[0] !== '/') name = '/' + name;
-
-            // Now actually add the alias
-            this.controlbox.preprocessor.aliases[name] = rule;
-        }
-    };
+        };
 
 
-    fn_to_bind['command:ignore'] = {
-        description: 'Ignore messages from somebody',
-        fn: function(ev) {
-            var that = this,
-                ignore_list = this.app.connections.active_connection.ignore_list,
-                user_mask;
+        fn_to_bind['command:set'] = {
+            description: translateText('command_description_set'),
+            fn: function(ev) {
+                if (!ev.params[0]) return;
 
-            // No parameters passed so list them
-            if (!ev.params[0]) {
-                if (ignore_list.length > 0) {
-                    this.app.panels().active.addMsg(' ', styleText('ignore_title', {text: translateText('client_models_application_ignore_title')}));
-                    ignore_list.forEach(function(ignored) {
-                        that.app.panels().active.addMsg(' ', styleText('ignored_pattern', {text: ignored.get('mask')}));
-                    });
-                } else {
-                    this.app.panels().active.addMsg(' ', styleText('ignore_none', {text: translateText('client_models_application_ignore_none')}));
+                var setting = ev.params[0],
+                    value;
+
+                // Do we have a second param to set a value?
+                if (ev.params[1]) {
+                    ev.params.shift();
+
+                    value = ev.params.join(' ');
+
+                    // If we're setting a true boolean value..
+                    if (value === 'true')
+                        value = true;
+
+                    // If we're setting a false boolean value..
+                    if (value === 'false')
+                        value = false;
+
+                    // If we're setting a number..
+                    if (parseInt(value, 10).toString() === value)
+                        value = parseInt(value, 10);
+
+                    _kiwi.global.settings.set(setting, value);
                 }
-                return;
+
+                // Read the value to the user
+                this.app.panels().active.addMsg('', styleText('set_setting', {text: setting + ' = ' + _kiwi.global.settings.get(setting).toString()}));
             }
-
-            // We have a parameter, so add it, first convert it to a full mask.
-            user_mask = toUserMask(ev.params[0]);
-            ignore_list.addMask(user_mask);
-
-            this.app.panels().active.addMsg(' ', styleText('ignore_nick', {text: translateText('client_models_application_ignore_nick', [user_mask])}));
-        }
-    };
+        };
 
 
-    fn_to_bind['command:unignore'] = {
-        description: 'Stop ignoring somebody',
-        fn: function(ev) {
-            var ignore_list = this.app.connections.active_connection.ignore_list,
-                user_mask, matches;
-
-            if (!ev.params[0]) {
-                this.app.panels().active.addMsg(' ', styleText('ignore_stop_notice', {text: translateText('client_models_application_ignore_stop_notice')}));
-                return;
+        fn_to_bind['command:save'] = {
+            description: translateText('command_description_save'),
+            fn: function(ev) {
+                _kiwi.global.settings.save();
+                this.app.panels().active.addMsg('', styleText('settings_saved', {text: translateText('client_models_application_settings_saved')}));
             }
+        };
 
-            user_mask = toUserMask(ev.params[0]);
-            matches = ignore_list.where({mask: user_mask});
-            if (matches) {
-                ignore_list.remove(matches);
+
+        fn_to_bind['command:alias'] = {
+            description: translateText('command_description_alias'),
+            fn: function(ev) {
+                var that = this,
+                    name, rule;
+
+                // No parameters passed so list them
+                if (!ev.params[1]) {
+                    $.each(this.controlbox.preprocessor.aliases, function (name, rule) {
+                        that.app.panels().server.addMsg(' ', styleText('list_aliases', {text: name + '   =>   ' + rule}));
+                    });
+                    return;
+                }
+
+                // Deleting an alias?
+                if (ev.params[0] === 'del' || ev.params[0] === 'delete') {
+                    name = ev.params[1];
+                    if (name[0] !== '/') name = '/' + name;
+                    delete this.controlbox.preprocessor.aliases[name];
+                    return;
+                }
+
+                // Add the alias
+                name = ev.params[0];
+                ev.params.shift();
+                rule = ev.params.join(' ');
+
+                // Make sure the name starts with a slash
+                if (name[0] !== '/') name = '/' + name;
+
+                // Now actually add the alias
+                this.controlbox.preprocessor.aliases[name] = rule;
             }
+        };
 
-            this.app.panels().active.addMsg(' ', styleText('ignore_stopped', {text: translateText('client_models_application_ignore_stopped', [user_mask])}));
-        }
-    };
+
+        fn_to_bind['command:ignore'] = {
+            description: translateText('command_description_ignore'),
+            fn: function(ev) {
+                var that = this,
+                    ignore_list = this.app.connections.active_connection.ignore_list,
+                    user_mask;
+
+                // No parameters passed so list them
+                if (!ev.params[0]) {
+                    if (ignore_list.length > 0) {
+                        this.app.panels().active.addMsg(' ', styleText('ignore_title', {text: translateText('client_models_application_ignore_title')}));
+                        ignore_list.forEach(function(ignored) {
+                            that.app.panels().active.addMsg(' ', styleText('ignored_pattern', {text: ignored.get('mask')}));
+                        });
+                    } else {
+                        this.app.panels().active.addMsg(' ', styleText('ignore_none', {text: translateText('client_models_application_ignore_none')}));
+                    }
+                    return;
+                }
+
+                // We have a parameter, so add it, first convert it to a full mask.
+                user_mask = toUserMask(ev.params[0]);
+                ignore_list.addMask(user_mask);
+
+                this.app.panels().active.addMsg(' ', styleText('ignore_nick', {text: translateText('client_models_application_ignore_nick', [user_mask])}));
+            }
+        };
+
+
+        fn_to_bind['command:unignore'] = {
+            description: translateText('command_description_unignore'),
+            fn: function(ev) {
+                var ignore_list = this.app.connections.active_connection.ignore_list,
+                    user_mask, matches;
+
+                if (!ev.params[0]) {
+                    this.app.panels().active.addMsg(' ', styleText('ignore_stop_notice', {text: translateText('client_models_application_ignore_stop_notice')}));
+                    return;
+                }
+
+                user_mask = toUserMask(ev.params[0]);
+                matches = ignore_list.where({mask: user_mask});
+                if (matches) {
+                    ignore_list.remove(matches);
+                }
+
+                this.app.panels().active.addMsg(' ', styleText('ignore_stopped', {text: translateText('client_models_application_ignore_stopped', [user_mask])}));
+            }
+        };
+
+
+        return fn_to_bind;
+    }
 
 
 
