54a061fe77bc803228804bb1b89487ca1842b693
Maxwell Dreytser <admin@mdtech.us>
2015-09-14 18:36:00 -0400

    Fix small bug.

--- client/src/views/member.js
+++ client/src/views/member.js
@@ -6,10 +6,11 @@ _kiwi.view.Member = Backbone.View.extend({
     },
     render: function () {
         var $this = this.$el,
-            prefix_css_class = (this.model.get('modes') || []).join(' ');
+            prefix_css_class = (this.model.get('modes') || []).join(' '),
+			max_prefix = (this.model.get('modes') || [])[0];
 
-        $this.attr('class', 'mode ' + prefix_css_class + ' member member-' + prefix_css_class);
-        $this.html('<a class="nick"><span class="prefix prefix-' + prefix_css_class + '">' + this.model.get("prefix") + '</span>' + this.model.get("nick") + '</a>');
+        $this.attr('class', 'mode ' + prefix_css_class + ' member member-' + max_prefix);
+        $this.html('<a class="nick"><span class="prefix prefix-' + max_prefix + '">' + this.model.get("prefix") + '</span>' + this.model.get("nick") + '</a>');
 
         return this;
     }
