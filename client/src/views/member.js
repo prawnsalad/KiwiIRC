f35e2a9e8d8719ca2e81869f47d9fe9181acc29e
Maxwell Dreytser <admin@mdtech.us>
2015-09-14 23:30:10 -0400

    Now if member has no prefix, we will use 'none'.

--- client/src/views/member.js
+++ client/src/views/member.js
@@ -7,6 +7,10 @@ _kiwi.view.Member = Backbone.View.extend({
     render: function () {
         var $this = this.$el,
 			max_prefix = (this.model.get('modes') || [])[0];
+		
+		if (!max_prefix) {
+			max_prefix = 'none';
+		}
 
         $this.attr('class', 'member member-' + max_prefix);
         $this.html('<a class="nick"><span class="prefix prefix-' + max_prefix + '">' + this.model.get("prefix") + '</span>' + this.model.get("nick") + '</a>');
