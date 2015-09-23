b7396559ce57fc009dbcc4df0f5da4f24b384949
Darren <darren@darrenwhitlen.com>
2015-09-23 23:02:34 +0100

    If no CIDR range provided for reverse proxy whitelist, assume single host

--- server/weblistener.js
+++ server/weblistener.js
@@ -145,6 +145,7 @@ function rangeCheck(addr, range) {
     ranges = (!_.isArray(range)) ? [range] : range;
     for (i = 0; i < ranges.length; i++) {
         parts = ranges[i].split('/');
+        parts[1] = parts[1] || '32'; // If no range provided, assume single host
         if (ipaddr.process(addr).match(ipaddr.process(parts[0]), parts[1])) {
             return true;
         }
