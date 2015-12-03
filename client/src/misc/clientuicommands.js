d0291aeea747a827e98223e400ab1a21790b9e44
Darren <darren@darrenwhitlen.com>
2015-12-03 19:03:28 +0000

    /mode command allowing reading+setting modes again

--- client/src/misc/clientuicommands.js
+++ client/src/misc/clientuicommands.js
@@ -457,12 +457,12 @@
         // Use the specified channel is one is given..
         if (network.isChannelName(ev.params[0])) {
             for_channel = ev.params[0];
-            params = ev.params.join(' ');
+            params = ev.params.slice(1).join(' ');
 
         // Use the current channel..
         } else if(panel.isChannel()) {
             for_channel = panel.get('name');
-            params = for_channel;
+            params = ev.params.join(' ');
 
         // Nothing to get a mode for..
         } else {
@@ -470,32 +470,34 @@
         }
 
         // Due to a flaw on the server-side we can't actually get modes for channels
-        // that we are not joined. 
-        network.gateway.on('channel_info', function onChanInfo(event) {
-            if (event.channel.toLowerCase() !== for_channel.toLowerCase() || typeof event.modes === 'undefined') {
-                return;
-            }
+        // that we are not joined.
+        if (!params) {
+            network.gateway.on('channel_info', function onChanInfo(event) {
+                if (event.channel.toLowerCase() !== for_channel.toLowerCase() || typeof event.modes === 'undefined') {
+                    return;
+                }
 
-            // No need to listen any more
-            network.gateway.off('channel_info', onChanInfo);
+                // No need to listen any more
+                network.gateway.off('channel_info', onChanInfo);
 
-            // Convert the modes into human readable format (+nt -xyz)
-            var mode_string = _.chain(event.modes)
-                .reduce(function(res, mode) {
-                    var type = mode.mode[0]==='-'?'-':'+';
-                    res[type].push(mode.mode.substr(1));
-                    return res;
-                }, {'+':[], '-':[]})
-                .reduce(function(res, modes, type) {
-                    if (modes.length > 0) res += type + modes.join('') + ' ';
-                    return res;
-                }, '')
-                .value();
-                
-            panel.addMsg('', event.channel + ' ' + mode_string);
-        });
+                // Convert the modes into human readable format (+nt -xyz)
+                var mode_string = _.chain(event.modes)
+                    .reduce(function(res, mode) {
+                        var type = mode.mode[0]==='-'?'-':'+';
+                        res[type].push(mode.mode.substr(1));
+                        return res;
+                    }, {'+':[], '-':[]})
+                    .reduce(function(res, modes, type) {
+                        if (modes.length > 0) res += type + modes.join('') + ' ';
+                        return res;
+                    }, '')
+                    .value();
+                    
+                panel.addMsg('', event.channel + ' ' + mode_string);
+            });
+        }
 
-        this.app.connections.active_connection.gateway.raw('MODE ' + params);
+        this.app.connections.active_connection.gateway.raw('MODE ' + for_channel + ' ' + params);
     }
 
 
